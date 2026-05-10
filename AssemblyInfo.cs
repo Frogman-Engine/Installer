56b9278a3adc5f9825f912f7d2076885eb647fcc
Unknown-Stryker <leehojin6580@gmail.com>
2026-05-11 02:26:13 +0900

    Update License

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,8 +1,4 @@
 using System.Windows;
-/*
-Copyright © from 2022 to present, UNKNOWN STRYKER. All Rights Reserved.
-Licensed under the AGPLv3 License. You may not use this file except in compliance with the License.
-*/
 
 
 
